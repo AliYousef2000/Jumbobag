4f9dbbfbe95da88217e3a283a144da47ad2b35c0
Ali Yousef <aliyousefaliali2000@gmail.com>
2023-03-08 21:38:05 +0200

    Finish home page

--- js.js
+++ js.js
@@ -2,6 +2,10 @@ let lip = document.getElementById("lipstick");
 let tool = document.getElementById("toolpic");
 window.addEventListener('scroll',function(){
     let value = window.scrollY;
-    lip.style.right= value/6 + 'px';
+    if(scrollY >= 919){
+        lip.style.right= value/6 + 'px';
+    }
+    if(scrollY >= 2990){
     tool.style.top= value/2 + 'px';
+    }
 })
